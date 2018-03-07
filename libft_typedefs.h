c31020dbb7712c7c8009efdac909fb9a01f4782c
Jonathan Levy <jolevy@e1z2r5p33.42.us.org>
2018-03-07 13:05:41 -0800

    jolevy test libft git

---
+++ libft_typedefs.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft_typedefs.h                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jolevy <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/02/28 11:46:51 by jolevy            #+#    #+#             */
+/*   Updated: 2018/02/28 11:47:04 by jolevy           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_TYPEDEFS_H
+# define LIBFT_TYPEDEFS_H
+
+typedef unsigned char	t_byte;
+typedef struct			s_list
+{
+	void				*content;
+	size_t				content_size;
+	struct s_list		*next;
+}						t_list;
+
+#endif
